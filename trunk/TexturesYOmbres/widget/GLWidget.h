66c223380831a5ff7361886af03e651182188bd5
mvm9289 <mvm9289@fb2e7589-4b86-6aa1-98f3-5b9a2f2df97d>
2010-10-15 09:36:00 +0000

    First commit of TexturesYOmbres

---
+++ trunk/TexturesYOmbres/widget/GLWidget.h
@@ -0,0 +1,94 @@
+#ifndef _GLWIDGET_H_
+#define _GLWIDGET_H_
+
+#include <QtOpenGL/qgl.h>
+#include <QtDesigner/QDesignerExportWidget>
+#include <QKeyEvent>
+#include <iostream>
+#include <QString>
+#include <QFileDialog>
+#include <QTimer>
+#include <ctime>
+
+#include "Point.h"
+#include "Scene.h"
+
+class  QDESIGNER_WIDGET_EXPORT GLWidget : public QGLWidget  // CAG
+{
+  Q_OBJECT
+
+ public:
+  //__declspec(dllexport) GLWidget(QWidget * parent);
+  GLWidget(QWidget * parent);
+  
+ signals:
+  void framerate(double);
+  void numQuads(double);
+  void numTriangles(double);
+
+ public slots:
+  // help - Ajuda per la terminal des de la que hem  engegat el programa.
+  void help(void);
+
+  // Afegiu aquí la declaració dels slots que necessiteu
+  void openModel();
+  void changeRenderMode(int mode);
+  void startStop();
+	void resetCamera();
+
+ protected:
+  // initializeGL() - Aqui incluim les inicialitzacions del contexte grafic.
+  virtual void initializeGL();
+
+  // paintGL - Mètode cridat cada cop que cal refrescar la finestra.
+  // Tot el que es dibuixa es dibuixa aqui.
+  virtual void paintGL( void );
+ 
+  // resizeGL() - Es cridat quan canvi la mida del widget
+  virtual void resizeGL (int width, int height);
+
+  // keyPressEvent() - Cridat quan es prem una tecla
+  virtual void keyPressEvent(QKeyEvent *e);
+
+  // mousePressEvent() i mouseReleaseEvent()
+  virtual void mousePressEvent( QMouseEvent *e);
+  virtual void mouseReleaseEvent( QMouseEvent *);
+ 
+  // mouseMoveEvent() - Cridat quan s'arrosega el ratoli amb algun botó premut.
+  virtual void mouseMoveEvent(QMouseEvent *e);
+ 
+  void computeCameraInicial(); 
+  void setModelview();
+  void setProjection();
+
+  void mostrarNumTrianglesQuads_Model();
+
+  Point getObs();
+  
+ private:
+  void computeCuttingPlanes();
+  
+  // paràmetres de la camera
+  float anglecam;
+  float ra;
+  double anterior, posterior, anteriorAux, posteriorAux, anteriorIni, posteriorIni; 
+  Point VRP;
+  float dist;
+  float angleX, angleY, angleZ;
+  
+  Scene escena;  // Escena a representar en el widget
+
+  // interaccio
+  typedef  enum {NONE, ROTATE, ZOOM, PAN} InteractiveAction;
+  InteractiveAction DoingInteractive;
+ 
+  int   xClick, yClick;
+
+  QTimer timer;
+  
+  clock_t oldTime;
+  int remainingFrames;
+  bool moviment;
+};
+
+#endif
